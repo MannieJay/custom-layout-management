ada80688d547a323c3a5f70742081feefd0a2de4
Joshua Hall <jhall02258@gmail.com>
2022-04-08 20:51:08 -0400

    inialize repo

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Styles/App.css';
+
+const App: React.FC = () => {
+    return (
+        <div className="app-container">
+            Hello World!
+        </div>
+    );
+}
+
+export default App;
